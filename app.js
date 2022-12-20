777992f1c0d44e3762dda420ae2547dea5db8c3b
jose <jrodriguez@itsolutionspcllc.com>
2022-12-20 18:21:09 -0500

    JavaScript_Variables

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ console.log(
 
 // Exercise 1
 
+const firstName = 'Jose';
+let lastName = 'Rodriguez';
+var age = 34;
+
+
 // Exercise 2
+let fullName ='Jose Rodriguez';
+console.log(fullName);
+
 
 // Exercise 3
+let pasTime = "Puerto Rico vacation";
+let city = "Fort Myers";
+let myStory =`\ hello my name is ${firstName} ${lastName} im ${age} 
+ years old from florida city of ${city} 
+ my favorite pastime was in ${pasTime} in 2018`;
+
+
+ console.log(myStory);
