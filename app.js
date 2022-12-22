89deb7bb76cb57e7d734a1741e223b1254800194
jose <jrodriguez@itsolutionspcllc.com>
2022-12-21 22:26:52 -0500

    completed excercise

--- app.js
+++ app.js
@@ -2,25 +2,71 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+//Working with Strings
+//Declare a variable named fullName using the let keyword, and assign your firstName plus your lastName as the string value
+//Print the value of fullName to the console (to open you browser console in Google Chrome, right-click and press "Inspect" or fn+F12)
+
+
+
+//Declare a variable named myStory using the let keyword, and assign a string with 3 sentences detailing:
+//An introduction using your fullName variable.
+//An introduction using your city variable.
+//An introduction using your favorite pastime variable.
+//Be sure to use variables as the above answers (declare and assign variables if you haven't already)
+//Print the result of myStory to the browser console
+
+
+
+
+
+
+
 
 // Exercise 1
 
-const firstName = 'Jose';
-let lastName = 'Rodriguez';
-var age = 34;
+const firstName = "Jose";
+let lastName;
+var age;
+let city;
+let favoritePastime;
 
 
 // Exercise 2
-let fullName ='Jose Rodriguez';
-console.log(fullName);
 
+lastName = "Rodriguez";
+age =35;
+city ="Fort Myers";
+favoritePastime = "Vacation in Puerto Rico";
+let fullName = `${firstName} ${lastName}`;
+console.log(`${fullName}`);
+
+let myStory = `Hi! I am ${fullName}  from south west Florida in the city of ${city} i am ${age} years old and my favorite pastime was the great ${favoritePastime}`
+console.log(myStory);
 
 // Exercise 3
-let pasTime = "Puerto Rico vacation";
-let city = "Fort Myers";
-let myStory =`\ hello my name is ${firstName} ${lastName} im ${age} 
- years old from florida city of ${city} 
- my favorite pastime was in ${pasTime} in 2018`;
+
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// 4 Declaring and Assigning Values to Variables
+
+let Price = 19.99;
+let Isonsale = false;
+let salePercentage =15;
+let stock = 0;
+let inStock = false;
+let selectedSize ="M";
 
 
- console.log(myStory);
+
+// 5 Declaring and Assigning Values to Variables
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+ console.log(`${language} was created in ${createdYear}`);
+ console.log(`${Price}`);
+ console.log(`${title}  Autho ${author} page count ${pageCount}`);
